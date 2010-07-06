fd13015ae800a961022adcb0157880def025e4d9
vfilippov <vfilippov@bab1fac5-469e-4ee1-a016-89f15f2f7d69>
2010-07-06 16:08:43 +0000

    Bug 51595 - Fixes for PHP 5.3.0

--- WikilogHooks.php
+++ WikilogHooks.php
@@ -139,7 +139,7 @@ class WikilogHooks
 	 * ArticleDeleteComplete hook handler function.
 	 * Purges wikilog metadata when an article is deleted.
 	 */
-	static function ArticleDeleteComplete( &$article, &$user, $reason, $id ) {
+	static function ArticleDeleteComplete( &$article, &$user, $reason, $id = NULL ) {
 		# Deleting comment through MW interface.
 		if ( $article instanceof WikilogCommentsPage ) {
 			$cmt =& $article->mSingleComment;
@@ -151,6 +151,8 @@ class WikilogHooks
 
 		# Retrieve wikilog information.
 		$wi = Wikilog::getWikilogInfo( $article->getTitle() );
+		if ( is_null($id) && $article )
+			$id = $article->getId();
 
 		# Take special procedures if it is a wikilog page.
 		if ( $wi ) {
