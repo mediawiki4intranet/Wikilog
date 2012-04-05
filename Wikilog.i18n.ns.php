e0602ddf43073dc3db9538f5b26694e1b467be3a
vfilippov <vfilippov@bab1fac5-469e-4ee1-a016-89f15f2f7d69>
2012-04-05 10:50:47 +0000

    Bug 92689 - Localise Wikilog namespaces

---
+++ Wikilog.i18n.ns.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Namespace names used by Wikilog extension for default Blog: namespace.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$namespaceNames = array();
+
+/** English (English) */
+$namespaceNames['en'] = array(
+	NS_BLOG => 'Blog',
+	NS_BLOG_TALK => 'Blog talk',
+);
+
+/** Russian (Русский) */
+$namespaceNames['ru'] = array(
+	NS_BLOG => 'Блог',
+	NS_BLOG_TALK => 'Обсуждение блога',
+);
