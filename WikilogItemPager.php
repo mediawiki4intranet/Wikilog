1f943f31827b03a2d8b39904e40d2713fff662c6
Stas Fomin <stas-fomin@ya.ru>
2026-03-06 16:03:44 +0300

    fix: Use getOptions()->getBool() instead of deprecated getBoolOption()

--- WikilogItemPager.php
+++ WikilogItemPager.php
@@ -110,7 +110,7 @@ class WikilogSummaryPager
 		# We will need a clean parser if not including.
 			$parser->startExternalParse( $wgTitle, $popt, Parser::OT_HTML );
 		}
-		$this->mShowEditLink = !$popt->getInterfaceMessage() && $this->getContext()->getUser()->getBoolOption( 'editsection' );
+		$this->mShowEditLink = !$popt->getInterfaceMessage() && $this->getContext()->getUser()->getOptions()->getBool( 'editsection' );
 	}
 
 	/**
